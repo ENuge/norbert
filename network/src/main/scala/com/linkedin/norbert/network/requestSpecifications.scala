00f7eaad920d9cc2bc4a5d45712da035b6f453ea
Helen <hwoodward@hmc.edu>
2014-11-13 10:30:03 -0800

    Added converters between partitioned and non-partitioned request spec
    Added converters between partitioned and non-partitioned request spec
    
    Added comments and renamed conversion methods from apply to convert so apply could still have defaults
    
    Removed impossible convert
    
    added comments
    
    Created necessary new interfaces and added new function def to the new interfaces, but didn't add it to the actualy implementations. Also added TODOs for all the places we need to add code/comments for the new function definition.
    Also renamed my requestSpec into RequestSpecification to match the interface

--- network/src/main/scala/com/linkedin/norbert/network/requestSpecifications.scala
+++ network/src/main/scala/com/linkedin/norbert/network/requestSpecifications.scala
@@ -1,28 +1,47 @@
 
-
 package com.linkedin.norbert.network
 import com.linkedin.norbert.cluster.{ClusterException, Node}
 
-object RequestSpec {
-  def apply[RequestMsg](message: RequestMsg): RequestSpec[RequestMsg] = {
-    new RequestSpec(message);
+/**
+ * A RequestSpecification object is used to store the necessary information to specify the request message
+ * For the non-partitioned version this is currently just the actual message
+ * You can either create the RequestSpec directly or convert a PartitionedRequestSpec
+ */
+
+
+object RequestSpecification {
+  def apply[RequestMsg](message: RequestMsg): RequestSpecification[RequestMsg] = {
+    new RequestSpecification(message);
   }
+
+  def convert[RequestMsg](partitionedSpec: PartitionedRequestSpecification[RequestMsg, _]): RequestSpecification[RequestMsg] = {
+    new RequestSpecification(partitionedSpec.message.get);
+  }
+
 }
 
-class RequestSpec[RequestMsg](val message: RequestMsg) {
+class RequestSpecification[RequestMsg](val message: RequestMsg) {
 
 }
 
+/**
+ * This is the partitioned version of RequestSpec. It serves the same purpose.
+ * In this partitioned version the request can be specified either by giving the actual method or by specifying a requestBuilder (rb)
+ * which will generate a message from a set of partitionedIds. At least one of those must be specified.
+ * You can also convert a RequestSpec into a ParitionedRequestSpec
+ */
 
-
-object PartitionedRequestSpec{
+object PartitionedRequestSpecification{
   def apply[RequestMsg, PartitionedId](message: Option[RequestMsg] = None,
-                                       rb: Option[(Node, Set[PartitionedId]) => RequestMsg] = None): PartitionedRequestSpec[RequestMsg, PartitionedId] = {
-    new PartitionedRequestSpec(message, rb)
+                                       rb: Option[(Node, Set[PartitionedId]) => RequestMsg] = None): PartitionedRequestSpecification[RequestMsg, PartitionedId] = {
+    new PartitionedRequestSpecification(message, rb)
+  }
+  def convert[RequestMsg, PartitionedId](requestSpec: RequestSpecification[RequestMsg]): PartitionedRequestSpecification[RequestMsg, PartitionedId] = {
+    new PartitionedRequestSpecification(Some(requestSpec.message), None)
   }
 }
 
-class PartitionedRequestSpec[RequestMsg, PartitionedId](val message: Option[RequestMsg],
+class PartitionedRequestSpecification[RequestMsg, PartitionedId](val message: Option[RequestMsg],
                                              val rb: Option[(Node, Set[PartitionedId]) => RequestMsg]) {
   if (message == None && rb == None) {
     //error if both message and requestBuilder are none
@@ -31,13 +50,18 @@ class PartitionedRequestSpec[RequestMsg, PartitionedId](val message: Option[Requ
 
 }
 
+/**
+ * Below is a main which provides some basic testing, it will most likely go away in the end
+ */
 
 object testing {
   def main(args: Array[String]) {
     try {
-      var tester = RequestSpec[String]("test");
-      var partitionedTester = PartitionedRequestSpec[String, Int]();
-      println("no error");
+      val tester: RequestSpecification[String] = RequestSpecification[String]("test")
+      val partitionedTester: PartitionedRequestSpecification[String, Int] = PartitionedRequestSpecification.convert[String, Int](tester)
+      val partitionedTester2: PartitionedRequestSpecification[String, Int] = PartitionedRequestSpecification[String, Int](Some("partitionedTest"))
+      val tester2: RequestSpecification[String] = RequestSpecification.convert[String](partitionedTester)
+      println(tester2.message);
     }
     catch {
       case e: Exception => println("There was an exception: " + e)
